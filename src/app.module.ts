c1a94624b424fc54d90d57c5c31216c5734e12b7
Faris Rayhan <frayhan94@gmail.com>
2024-07-31 08:23:30 +0700

    feat: use env variable for mysql connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { ItemModule } from './items/item.module';
 import { FavoriteItemModule } from './favourite-item/favourite-item.module';
@@ -9,19 +10,26 @@ import { FavoriteItem } from './favourite-item/favorite-item.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'password',
-      database: 'inventory',
-      entities: [User,FavoriteItem, Item],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true, // makes the config module globally available
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: configService.get<string>('DATABASE_TYPE') as any,
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USERNAME'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        entities: [User, FavoriteItem, Item],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
     AuthModule,
     ItemModule,
-    FavoriteItemModule
+    FavoriteItemModule,
   ],
 })
 export class AppModule {}
