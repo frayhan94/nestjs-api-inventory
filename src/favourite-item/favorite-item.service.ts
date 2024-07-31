4f584989eb5bdf39af3ace046d7845da8c69c16a
Faris Rayhan <frayhan94@gmail.com>
2024-07-31 07:49:22 +0700

    fix: fix missing user id

--- src/favourite-item/favorite-item.service.ts
+++ src/favourite-item/favorite-item.service.ts
@@ -3,21 +3,30 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { FavoriteItem } from './favorite-item.entity';
 import { CreateFavoriteItemDto } from './favorite-item.dto';
+import { User } from '../auth/user.entity';
 
 @Injectable()
 export class FavoriteItemService {
   constructor(
     @InjectRepository(FavoriteItem)
     private favoriteItemRepository: Repository<FavoriteItem>,
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
   ) {}
 
-  create(createFavoriteItemDto: CreateFavoriteItemDto): Promise<FavoriteItem> {
-    const favoriteItem = this.favoriteItemRepository.create(createFavoriteItemDto);
+  async create(createFavoriteItemDto: CreateFavoriteItemDto): Promise<FavoriteItem> {
+    const { userId, itemId } = createFavoriteItemDto;
+    const user = await this.userRepository.findOne({ where: { id: userId } });
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    const favoriteItem = this.favoriteItemRepository.create({ itemId, user });
     return this.favoriteItemRepository.save(favoriteItem);
   }
 
   findAll(): Promise<FavoriteItem[]> {
-    return this.favoriteItemRepository.find();
+    return this.favoriteItemRepository.find({ relations: ['user'] });
   }
 
   async remove(id: number): Promise<void> {
--- src/favourite-item/favourite-item.module.ts
+++ src/favourite-item/favourite-item.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { FavoriteItem } from './favorite-item.entity';
 import { FavoriteItemService } from './favorite-item.service';
 import { FavoriteItemController } from './favorite-item.controller';
+import { User } from '../auth/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([FavoriteItem])],
+  imports: [TypeOrmModule.forFeature([FavoriteItem,User])],
   providers: [FavoriteItemService],
   controllers: [FavoriteItemController],
 })
